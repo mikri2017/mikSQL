b1265a5366bbd75be88ef4599c4f9b80e734a58a
MIKRI <2007mik007@mail.ru>
2020-05-09 12:56:02 +0700

    Designed program forms.
    Набросал примерный внешний вид приложения.

---
+++ aboutprog.pas
@@ -0,0 +1,39 @@
+unit AboutProg;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs,
+  ButtonPanel, StdCtrls;
+
+type
+
+  { TfrmAboutProg }
+
+  TfrmAboutProg = class(TForm)
+    ButtonPanel1: TButtonPanel;
+    lblDevEmail: TLabel;
+    lblDevName: TLabel;
+    lblVersion: TLabel;
+    lblCapVersion: TLabel;
+    lblCapDevName: TLabel;
+    lblProgName: TLabel;
+    lblVersionValue: TLabel;
+    lblIconsPartner: TLabel;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  frmAboutProg: TfrmAboutProg;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
--- mainfrm.pas
+++ mainfrm.pas
@@ -5,10 +5,39 @@ unit mainFrm;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, StdCtrls,
+  ComCtrls, ExtCtrls, ButtonPanel, VirtualTrees, AboutProg;
 
 type
+
+  { TfrmMain }
+
   TfrmMain = class(TForm)
+    btnStartQuery: TButton;
+    grpbxCon: TGroupBox;
+    mQueryStr: TMemo;
+    mItmConExit: TMenuItem;
+    mItmConSave: TMenuItem;
+    mItmConAdd: TMenuItem;
+    mItmAbout: TMenuItem;
+    mItmHelp: TMenuItem;
+    menuMain: TMainMenu;
+    mItmCon: TMenuItem;
+    pCtrlQuery: TPageControl;
+    pnlQueryCtrl: TPanel;
+    pnlQueruCtrls: TPanel;
+    splCons: TSplitter;
+    splQuery: TSplitter;
+    stBrInfo: TStatusBar;
+    tsQuery: TTabSheet;
+    vstConnections: TVirtualStringTree;
+    vstLog: TVirtualStringTree;
+    vstQResults: TVirtualStringTree;
+    procedure btnStartQueryClick(Sender: TObject);
+    procedure mItmAboutClick(Sender: TObject);
+    procedure mItmConAddClick(Sender: TObject);
+    procedure mItmConExitClick(Sender: TObject);
+    procedure mItmConSaveClick(Sender: TObject);
   private
 
   public
@@ -21,6 +50,44 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TfrmMain }
+
+procedure TfrmMain.mItmConExitClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TfrmMain.mItmConSaveClick(Sender: TObject);
+begin
+  ShowMessage('Функционал в разработке...');
+end;
+
+procedure TfrmMain.mItmAboutClick(Sender: TObject);
+begin
+  if Assigned(frmAboutProg) then
+     FreeAndNil(frmAboutProg);
+
+  Application.CreateForm(TfrmAboutProg, frmAboutProg);
+
+  frmAboutProg.lblProgName.Caption := 'mikSQL';
+  frmAboutProg.lblVersion.Caption := '0.1';
+
+  frmAboutProg.lblDevName.Caption := 'Михаил Рыжков MIKRI';
+  frmAboutProg.lblDevEmail.Caption := '2007mik007@mail.ru';
+
+  frmAboutProg.ShowModal;
+end;
+
+procedure TfrmMain.btnStartQueryClick(Sender: TObject);
+begin
+  ShowMessage('Функционал в разработке...');
+end;
+
+procedure TfrmMain.mItmConAddClick(Sender: TObject);
+begin
+  ShowMessage('Функционал в разработке...');
+end;
 
 end.
 
